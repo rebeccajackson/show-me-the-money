7419c95480956070fdee2b115f00b355a810a3dc
JamesCrimp <james.crimp@gmail.com>
2019-02-21 14:39:03 +1300

    Add seed data

---
+++ seeds/populateMeetings.js
@@ -0,0 +1,13 @@
+
+exports.seed = function(knex, Promise) {
+  // Deletes ALL existing entries
+  return knex('meetings').del()
+    .then(function () {
+      // Inserts seed entries
+      return knex('meetings').insert([
+        {id: 1, title: 'Fiddy cent club', owner_id: 1, startTime: 1550710530719, endTime: 1550711330719, duration: 800000},
+        {id: 2, title: 'Big important chats', owner_id: 1, startTime: 1550711483447, endTime: 1550711963447, duration: 480000},
+        {id: 3, title: 'Spensive meat', owner_id: 1, startTime: 1550668444000, endTime: 1550669999000, duration: 1555000}
+      ]);
+    });
+};
