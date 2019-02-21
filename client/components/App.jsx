df3a24e90b850248d4fb86f2fb2852777202479c
Nat Grant <email.natgrant@gmail.com>
2019-02-22 00:58:54 +1300

    updating location of History container

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,33 +1,30 @@
-import React from 'react'
-import { HashRouter as Router, Route, Link } from 'react-router-dom'
-import { connect } from 'react-redux'
+import React from "react";
+import { HashRouter as Router, Route, Link } from "react-router-dom";
+import { connect } from "react-redux";
 
-import Login from './Login'
-import Register from './Register'
-import Nav from './Nav'
-import Meeting from '../containers/Meeting'
-import NewMeeting from '../containers/NewMeeting'
-import History from './History'
-import PostMeeting from './PostMeeting';
+import Login from "./Login";
+import Register from "./Register";
+import Nav from "./Nav";
+import Meeting from "../containers/Meeting";
+import NewMeeting from "../containers/NewMeeting";
+import History from "../containers/History";
+import PostMeeting from "./PostMeeting";
 
 export function App({ auth }) {
   return (
     <Router>
       <div className="container has-text-centered">
-
         <div className="hero is-small is-primary">
           <div className="hero-body has-text-centered">
-            <Link to='/' className="">
+            <Link to="/" className="">
               <h1 className="title is-1">$how Me The Money</h1>
             </Link>
             <Nav />
           </div>
         </div>
 
-        <div className=''>
-          {!auth.isAuthenticated &&
-            <Route exact path="/" component={Login} />
-          }
+        <div className="">
+          {!auth.isAuthenticated && <Route exact path="/" component={Login} />}
           <Route path="/login" component={Login} />
           <Route path="/register" component={Register} />
           <Route path="/newmeeting" component={NewMeeting} />
@@ -35,16 +32,15 @@ export function App({ auth }) {
           <Route path="/history" component={History} />
           <Route path="/meeting/:meetingId" component={PostMeeting} />
         </div>
-
       </div>
     </Router>
-  )
+  );
 }
 
 const mapStateToProps = ({ auth }) => {
   return {
     auth
-  }
-}
+  };
+};
 
-export default connect(mapStateToProps)(App)
+export default connect(mapStateToProps)(App);
