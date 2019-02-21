497ed7d0bf6b3f4d1aa288240795ffe77b3f0fe2
Rebecca Jackson <zimbecs@gmail.com>
2019-02-21 15:51:05 +1300

    getAllUsers and getMeetingHistory functions are working

--- server/db/db.js
+++ server/db/db.js
@@ -10,27 +10,27 @@ module.exports = {
 function getMeetingHistory (testDb) {
   const db = testDb || connection
   return db('meetings')
-  .join('meetings-users', 'meetings-users.meeting_id', 'meeting')
-  .join('users', 'users.id', 'meetings-users.user_id')
-  select().orderBy('meetings.id', 'desc')
+  .join('meetings_users', 'meetings_users.meeting_id', 'meetings.id')
+  .join('users', 'users.id', 'meetings_users.user_id')
+  .select().orderBy('meetings.id', 'desc')
 }
 
 function getUsersByMeetingId(meetingId, testDb){
   const db = testDb || connection
   return db('meetings')
-  .join('meetings-users', 'meetings-users.meetings_id', 'meetings.id')
-  .where('meetings-users.meetings_id', meetingId)
+  .join('meetings_users', 'meetings_users.meetings_id', 'meetings.id')
+  .where('meetings_users.meetings_id', meetingId)
   .select('users.first_name as firstName', 'users.last_name as lastName', 'users.hourly_wage as hourlyWage').orderBy('name')
 }
 
 function saveMeeting(testDb){
   const db = testDb || connection
-  return('meetings')
-  // .join('meetings-users')
+  return db('meetings')
+  // .join('meetings_users')
 }
 
 function getAllUsers(testDb){
   const db = testDb || connection
-  return('users')
-  .select('users.first_name as firstName', 'users.last_name as lastName', 'users.hourly_wage as hourlyWage').orderBy('name')
+  return db('users')
+  .select('users.first_name as firstName', 'users.last_name as lastName', 'users.hourly_wage as hourlyWage')
 }
