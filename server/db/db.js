7264ecd448ea0d9b1fa92ff4b52b7936283f3f28
Rebecca Jackson <zimbecs@gmail.com>
2019-02-21 14:30:29 +1300

    db file added

---
+++ server/db/db.js
@@ -0,0 +1,22 @@
+const connection = require('./connection')
+
+module.exports = {
+  getMeetingHistory,
+  saveMeeting,
+  getUsersByMeetingId,
+  getAllUsers  
+}
+
+function getMeetingHistory (testDb) {
+  const db = testDb || connection
+  return db('meetings')
+  .join('meetings-users', 'meetings-users.meeting_id', 'meeting')
+
+}
+
+// function getMonthVeges(monthId, db = connection){
+//   return db('veg_months')
+//   .where('veg_months.month_id', monthId)
+//   .join('veg', 'veg.id', 'veg_months.veg_id')
+//   .select().orderBy('name')
+// }
